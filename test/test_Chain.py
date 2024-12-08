ac2e8925738d76fdc66e0b5d407bd6f97b525330
Tom Newson <newsontom@outlook.com>
2024-12-08 13:38:26 +0000

    use chain detection for direct win detection

--- test/test_Chain.py
+++ test/test_Chain.py
@@ -4,12 +4,8 @@ from src.Board import Board
 from src.Colour import Colour
 from agents.Group17.chain import ChainFinder
 
-
-# NOTE: LLM generated tests not checked by human
-
-
-class TestWin(unittest.TestCase):
-    def test_11x11_with_virtual_connection_and_winning_chain(self):
+class TestChain(unittest.TestCase):
+    def test_red_side_wiggle(self):
         board_str = (
         """ R 0 0 0 0 0 0 0 R R R
              0 R 0 0 0 0 0 0 0 0 0
@@ -25,12 +21,12 @@ class TestWin(unittest.TestCase):
         )
         board = Board.from_string(board_str, board_size=11)
         self.assertEqual(board.size, 11)
-        
+
         tiles = [[tile.colour for tile in row] for row in board.tiles]
         chain_finder = ChainFinder(tiles, Colour.RED)
         found, virtuals = chain_finder.search()
         self.assertTrue(found)
-        self.assertListEqual(virtuals, [((0,1), (1,0))])
+        self.assertCountEqual(virtuals, [((1,0), (0,1)),((2,1), (2,0))])
 
     def test_blue(self):
         board_str = (
@@ -48,37 +44,355 @@ class TestWin(unittest.TestCase):
         )
         board = Board.from_string(board_str, board_size=11)
         self.assertEqual(board.size, 11)
-        
+
         tiles = [[tile.colour for tile in row] for row in board.tiles]
         chain_finder = ChainFinder(tiles, Colour.BLUE)
         found, virtuals = chain_finder.search()
         self.assertTrue(found)
-        self.assertListEqual(virtuals, [((2,5), (3,4))])
+        self.assertCountEqual(virtuals, [((2,5), (3,4))])
 
-    def test_x(self):
+    def test_blue_right_drop_wiggle(self):
         board_str = (
-        """R R R R R R R R R R R 
-        R R B R R R R R B R R 
-        R R R R 0 0 0 B 0 0 0 
-        0 0 0 0 0 B 0 0 0 B 0 
-        0 B 0 B B B B 0 0 0 B 
-        0 0 0 0 0 0 B B 0 0 0 
-        0 0 0 0 0 B 0 0 B B 0 
-        0 0 0 B B R 0 0 0 0 0 
-        0 0 B 0 0 B 0 0 0 0 0 
-        0 0 0 0 0 0 0 0 0 0 0 
-        B B B 0 0 0 0 0 0 B 0"""
+        """ R R R R R R R R R R R
+             R R B R R R R R B R R
+              R R R R 0 0 0 B 0 0 0
+               0 0 0 0 0 B 0 0 0 B 0
+                0 B 0 B B B B 0 0 0 B
+                 0 0 0 0 0 0 B B 0 0 0
+                  0 0 0 0 0 B 0 0 B B 0
+                   0 0 0 B B R 0 0 0 0 0
+                    0 0 B 0 0 B 0 0 0 0 0
+                     0 0 0 0 0 0 0 0 0 0 0
+                      B B B 0 0 0 0 0 0 B 0"""
         )
         board = Board.from_string(board_str, board_size=11)
         self.assertEqual(board.size, 11)
-        
+
         tiles = [[tile.colour for tile in row] for row in board.tiles]
         chain_finder = ChainFinder(tiles, Colour.BLUE)
         found, virtuals = chain_finder.search()
         self.assertTrue(found)
-        self.assertListEqual(virtuals, [((2,5), (3,4))])
+        self.assertCountEqual(virtuals, [((9,1), (9,2)), ((5,8), (6,7)), ((5,9), (5,10))])
 
+    def test_empty(self):
+        board_str = (
+        """0 0 0 0 0 0 0 0 0 0 0
+ 0 0 0 0 0 0 0 0 0 0 0
+  0 0 0 0 0 0 0 0 0 0 0
+   0 0 0 0 0 0 0 0 0 0 0
+    0 0 0 0 0 0 0 0 0 0 0
+     0 0 0 0 0 0 0 0 0 0 0
+      0 0 0 0 0 0 0 0 0 0 0
+       0 0 0 0 0 0 0 0 0 0 0
+        0 0 0 0 0 0 0 0 0 0 0
+         0 0 0 0 0 0 0 0 0 0 0
+          0 0 0 0 0 0 0 0 0 0 0 """
+        )
+        board = Board.from_string(board_str, board_size=11)
+        self.assertEqual(board.size, 11)
 
+        tiles = [[tile.colour for tile in row] for row in board.tiles]
+        chain_finder = ChainFinder(tiles, Colour.RED)
+        found, virtuals = chain_finder.search()
+        self.assertFalse(found)
+
+    def test_wiggle(self):
+        board_str = (
+        """0 0 0 0 0 0 0 0 0 R 0
+        0 0 0 0 0 0 0 0 0 0 0
+        0 0 0 0 0 0 0 0 R 0 0
+        0 0 0 0 0 0 R 0 0 0 0
+        0 0 0 0 R 0 0 0 0 0 0
+        0 0 R 0 0 0 0 0 0 0 0
+        R 0 0 0 0 0 0 0 0 0 0
+        0 R 0 0 0 0 0 0 0 0 0
+        0 0 R 0 0 0 0 0 0 0 0
+        0 0 0 R 0 0 0 0 0 0 0
+        0 0 0 0 R 0 0 0 0 0 0 """
+        )
+        board = Board.from_string(board_str, board_size=11)
+        self.assertEqual(board.size, 11)
+        tiles = [[tile.colour for tile in row] for row in board.tiles]
+        chain_finder = ChainFinder(tiles, Colour.RED)
+        found, virtuals = chain_finder.search()
+        self.assertTrue(found)
+
+    def test_wiggle_blocked(self):
+        board_str = (
+        """0 0 0 0 0 0 0 0 0 R 0
+        0 0 0 0 0 0 0 0 0 0 0
+        0 0 0 0 0 0 0 0 R 0 0
+        0 0 0 0 0 0 R 0 0 0 0
+        0 0 0 0 R 0 0 0 0 0 0
+        0 0 R 0 0 0 0 0 0 0 0
+        R 0 0 0 0 0 0 0 0 0 0
+        0 R 0 0 0 0 0 0 0 0 0
+        0 0 R 0 0 0 0 0 0 0 0
+        0 0 0 R R 0 0 0 0 0 0
+        0 0 B B B 0 0 0 0 0 0 """
+        )
+        board = Board.from_string(board_str, board_size=11)
+        self.assertEqual(board.size, 11)
+        tiles = [[tile.colour for tile in row] for row in board.tiles]
+        chain_finder = ChainFinder(tiles, Colour.RED)
+        found, virtuals = chain_finder.search()
+        self.assertFalse(found)
+
+    @unittest.skip("Edge detection not implemented")
+    def test_top_virtual(self):
+        board_str = (
+        """0 0 0 0 0 0 0 0 0 0 0
+        0 0 0 0 0 R R R R R 0
+        0 0 0 0 0 0 0 0 R 0 0
+        0 0 0 0 0 0 R 0 0 0 0
+        0 0 0 0 R 0 0 0 0 0 0
+        0 0 R 0 0 0 0 0 0 0 0
+        R 0 0 0 0 0 0 0 0 0 0
+        0 R 0 0 0 0 0 0 0 0 0
+        0 0 R 0 0 0 0 0 0 0 0
+        0 0 0 R R 0 0 0 0 0 0
+        0 0 B B B 0 0 0 0 0 0 """
+        )
+        board = Board.from_string(board_str, board_size=11)
+        self.assertEqual(board.size, 11)
+        tiles = [[tile.colour for tile in row] for row in board.tiles]
+        chain_finder = ChainFinder(tiles, Colour.RED)
+        found, virtuals = chain_finder.search()
+        self.assertTrue(found)
+
+    @unittest.skip("Edge detection not implemented")
+    def test_bottom_virtual(self):
+        board_str = (
+        """0 0 0 0 0 0 R 0 0 0 0
+        0 0 0 0 0 R R R R R 0
+        0 0 0 0 0 0 0 0 R 0 0
+        0 0 0 0 0 0 R 0 0 0 0
+        0 0 0 0 R 0 0 0 0 0 0
+        0 0 R 0 0 0 0 0 0 0 0
+        R 0 0 0 0 0 0 0 0 0 0
+        0 R 0 0 0 0 0 0 0 0 0
+        0 0 R 0 0 0 0 0 0 0 0
+        0 0 0 R R R R R R 0 0
+        0 0 B B B 0 0 0 0 0 0 """
+        )
+        board = Board.from_string(board_str, board_size=11)
+        self.assertEqual(board.size, 11)
+        tiles = [[tile.colour for tile in row] for row in board.tiles]
+        chain_finder = ChainFinder(tiles, Colour.RED)
+        found, virtuals = chain_finder.search()
+        self.assertTrue(found)
+
+    def test_blue_virtual(self):
+        board_str = (
+        """ B B B 0 0 0 R 0 0 0 0
+            0 0 0 B B B B B B B B
+            0 0 0 0 0 0 0 0 R 0 0
+            0 0 0 0 0 0 R 0 0 0 0
+            0 0 0 0 R 0 0 0 0 0 0
+            0 0 R 0 0 0 0 0 0 0 0
+            R 0 0 0 0 0 0 0 0 0 0
+            0 R 0 0 0 0 0 0 0 0 0
+            0 0 R 0 0 0 0 0 0 0 0
+            0 0 0 R R R R R R 0 0
+            0 0 B B B 0 0 0 0 0 0 """
+        )
+        board = Board.from_string(board_str, board_size=11)
+        self.assertEqual(board.size, 11)
+        tiles = [[tile.colour for tile in row] for row in board.tiles]
+        chain_finder = ChainFinder(tiles, Colour.BLUE)
+        found, virtuals = chain_finder.search()
+        self.assertTrue(found)
+
+    @unittest.skip("Edge detection not implemented")
+    def test_blue_right_finish(self):
+        board_str = (
+        """ R R R R R R R R R R R
+            R R R R R B R B R R 0
+            B 0 0 0 0 B 0 0 0 0 0
+            0 0 0 0 0 B B 0 0 0 0
+            0 0 0 0 0 B B 0 0 B 0
+            0 0 0 0 0 0 0 0 0 0 0
+            0 0 0 0 B B B B B B 0
+            0 0 0 B R 0 0 0 0 0 0
+            B B B 0 0 0 0 0 0 0 0
+            0 0 0 0 0 0 0 0 0 0 0
+            0 0 0 0 0 0 0 0 0 0 0"""
+        )
+        board = Board.from_string(board_str, board_size=11)
+        self.assertEqual(board.size, 11)
+        tiles = [[tile.colour for tile in row] for row in board.tiles]
+        chain_finder = ChainFinder(tiles, Colour.BLUE)
+        found, virtuals = chain_finder.search()
+        self.assertTrue(found)
+
+    @unittest.skip("Edge detection not implemented")
+    def test_blue_left_finish(self):
+        board_str = (
+        """ R R R R R R R R R R R
+            R R R R R B R B R R 0
+            B 0 0 0 0 B 0 0 0 0 0
+            0 0 0 0 0 B B 0 0 0 0
+            0 0 0 0 0 B B 0 0 B 0
+            0 0 0 0 0 0 0 0 0 0 0
+            0 0 0 0 B B B B B B B
+            0 0 0 B R 0 0 0 0 0 0
+            0 B B 0 0 0 0 0 0 0 0
+            0 0 0 0 0 0 0 0 0 0 0
+            0 0 0 0 0 0 0 0 0 0 0"""
+        )
+        board = Board.from_string(board_str, board_size=11)
+        self.assertEqual(board.size, 11)
+        tiles = [[tile.colour for tile in row] for row in board.tiles]
+        chain_finder = ChainFinder(tiles, Colour.BLUE)
+        found, virtuals = chain_finder.search()
+        self.assertTrue(found)
+
+    def test_red_full(self):
+        board_str = (
+        """ R R R R R R R R R R R
+            R R R R R B R B R R 0
+            B 0 0 0 R B 0 0 0 0 0
+            0 0 0 0 R B B 0 0 0 0
+            0 0 0 0 R B B 0 0 B 0
+            0 0 0 0 R 0 0 0 0 0 0
+            0 0 0 0 R B B B B B B
+            0 0 0 B R 0 0 0 0 0 0
+            0 B B 0 R 0 0 0 0 0 0
+            0 0 0 0 R 0 0 0 0 0 0
+            0 0 0 0 R 0 0 0 0 0 0"""
+        )
+        board = Board.from_string(board_str, board_size=11)
+        self.assertEqual(board.size, 11)
+        tiles = [[tile.colour for tile in row] for row in board.tiles]
+        chain_finder = ChainFinder(tiles, Colour.RED)
+        found, virtuals = chain_finder.search()
+        self.assertTrue(found)
+
+    def test_red_full_virtuals_disabled(self):
+        board_str = (
+        """ R R R R R R R R R R R
+            R R R R R B R B R R 0
+            B 0 0 0 R B 0 0 0 0 0
+            0 0 0 0 R B B 0 0 0 0
+            0 0 0 0 R B B 0 0 B 0
+            0 0 0 0 R 0 0 0 0 0 0
+            0 0 0 0 R B B B B B B
+            0 0 0 B R 0 0 0 0 0 0
+            0 B B 0 R 0 0 0 0 0 0
+            0 0 0 0 R 0 0 0 0 0 0
+            0 0 0 0 R 0 0 0 0 0 0"""
+        )
+        board = Board.from_string(board_str, board_size=11)
+        self.assertEqual(board.size, 11)
+        tiles = [[tile.colour for tile in row] for row in board.tiles]
+        chain_finder = ChainFinder(tiles, Colour.RED)
+        found, virtuals = chain_finder.search(False)
+        self.assertTrue(found)
+
+    def test_blue_full(self):
+        board_str = (
+        """ R R R R R R R R R R R
+            R R R R R B R B R R 0
+            B 0 0 0 R B 0 0 0 0 0
+            0 0 0 0 R B B 0 0 0 0
+            0 0 0 0 R B B 0 0 B 0
+            0 0 0 0 R 0 0 0 0 0 0
+            B B B B B B B B B B B
+            0 0 0 B R 0 0 0 0 0 0
+            0 B B 0 R 0 0 0 0 0 0
+            0 0 0 0 R 0 0 0 0 0 0
+            0 0 0 0 R 0 0 0 0 0 0"""
+        )
+        board = Board.from_string(board_str, board_size=11)
+        self.assertEqual(board.size, 11)
+        tiles = [[tile.colour for tile in row] for row in board.tiles]
+        chain_finder = ChainFinder(tiles, Colour.BLUE)
+        found, virtuals = chain_finder.search()
+        self.assertTrue(found)
+
+    def test_blue_full_wiggle(self):
+        board_str = (
+        """R R R R R R B R B R R
+            R R R R R R R R R R R
+             B R B R R B R R B B B
+              R R B B 0 0 0 B B 0 B
+               B 0 0 0 0 0 0 0 0 0 0
+                0 0 0 0 0 0 0 0 0 0 0
+                 0 R 0 0 B 0 0 0 0 B B
+                  0 B 0 0 0 0 0 B B 0 0
+                   0 0 B B B B B 0 0 0 0
+                    B B 0 0 0 0 0 0 0 0 0
+                     0 0 0 0 0 0 0 0 0 B 0"""
+        )
+        board = Board.from_string(board_str, board_size=11)
+        self.assertEqual(board.size, 11)
+        tiles = [[tile.colour for tile in row] for row in board.tiles]
+        chain_finder = ChainFinder(tiles, Colour.BLUE)
+        found, virtuals = chain_finder.search()
+        self.assertTrue(found)
+
+    def test_blue_no_winning_chain(self):
+        board_str = (
+            """ R R R R R R R R R R R
+                  R R R R R B R R R B R
+                    R R R R B R R B B R R
+                      B B B R R B B B B R B
+                        0 B 0 B B B B B B B 0
+                          0 0 0 0 B 0 B B B B 0
+                            0 0 R B 0 0 0 0 0 0 0
+                              0 0 B 0 0 0 0 0 0 0 0
+                                0 0 0 B 0 0 0 0 0 0 0
+                                  0 0 0 0 0 0 0 0 0 0 0
+                                    B 0 0 B 0 0 0 0 0 0 0"""
+        )
+        board = Board.from_string(board_str, board_size=11)
+        self.assertEqual(board.size, 11)
+        tiles = [[tile.colour for tile in row] for row in board.tiles]
+        chain_finder = ChainFinder(tiles, Colour.BLUE)
+        found, virtuals = chain_finder.search()
+        self.assertFalse(found)
+
+    def test_blue_no_winning_chain2(self):
+        board_str = (
+            """ R R R R R R R R R R R
+                  R R R R R B R R R B R
+                    R R R R B R 0 B B 0 0
+                      B B B 0 0 0 B 0 B 0 B
+                        0 B 0 0 B B 0 B 0 B 0
+                          0 0 0 0 B 0 0 B B B 0
+                0 0 R B 0 0 0 0 0 0 0
+                0 0 B 0 0 0 0 0 0 0 0
+                0 0 0 B 0 0 0 0 0 0 0
+                0 0 0 0 0 0 0 0 0 0 0
+                B 0 0 B 0 0 0 0 0 0 0"""
+        )
+        board = Board.from_string(board_str, board_size=11)
+        self.assertEqual(board.size, 11)
+        tiles = [[tile.colour for tile in row] for row in board.tiles]
+        chain_finder = ChainFinder(tiles, Colour.BLUE)
+        found, virtuals = chain_finder.search()
+        self.assertFalse(found)
+
+    def test_blue_no_winning_chain3(self):
+        board_str = (
+            """R R R R R R R R R R R
+ R R R R R R R B B B R
+  R R R B R R R R R B R
+   R R B 0 0 R B 0 0 0 0
+    B 0 0 B B B 0 0 B 0 0
+     0 0 0 B 0 0 B B 0 0 B
+      0 0 B 0 B 0 0 B 0 0 0
+       0 0 0 0 B B B B B 0 0
+        0 0 0 0 0 0 0 0 0 B 0
+         0 B 0 0 0 0 0 0 0 0 B
+          0 B 0 B 0 0 0 B 0 0 0 """
+        )
+        board = Board.from_string(board_str, board_size=11)
+        self.assertEqual(board.size, 11)
+        tiles = [[tile.colour for tile in row] for row in board.tiles]
+        chain_finder = ChainFinder(tiles, Colour.BLUE)
+        found, virtuals = chain_finder.search()
+        self.assertFalse(found)
 
 if __name__ == "__main__":
     unittest.main()
